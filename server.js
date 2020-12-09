d2d3b7f4f4f0198b433e1442e823f08ef58fe3ae
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-12-09 20:02:16 +0530

    successfully conneted to DB

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const dotenv = require('dotenv');
 const app = require('./app');
   
 dotenv.config({ path: './.env' });
@@ -8,7 +7,7 @@ dotenv.config({ path: './.env' });
 const port = process.env.PORT || 8000;
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  '<password>',
   process.env.DATABASE_PASSWORD
 );
 
@@ -16,7 +15,8 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
   .then(() => console.log('DB connection successful!'));
 
