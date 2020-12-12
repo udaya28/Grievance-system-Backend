aa4e18156c7ed06643492b869e6e7f8d5bf1e09d
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-12-12 15:15:45 +0530

    added cookie

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const morgan = require('morgan');
 const adminRouter = require('./src/router/adminRoute')
 const studentRouter = require('./src/router/studentRoute');
 const superUserRouter = require('./src/router/superUserRoute');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cookieParser());
 app.use((req, res, next) => {
   console.log('  🍀  🍀  🍀  🍀  🍀  ');
   next();
@@ -15,4 +17,20 @@ app.use((req, res, next) => {
 app.use('/admin',adminRouter);
 app.use('/student',studentRouter)
 app.use('/superUser',superUserRouter)
+
+
+app.get('/set-cookies',(req,res)=>{
+  
+  res.cookie('name','udaya vivek',{maxAge:10000000,httpOnly:true,})
+  res.send('send')
+})
+
+app.get('/get-cookies',(req,res)=>{
+  
+  const cookie = req.cookies
+  res.send(cookie)
+})
+
+
+
 module.exports = app;
